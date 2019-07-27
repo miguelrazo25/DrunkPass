9fa6e62607205a3790e79cd3478db65287a6bf5d
TacticalLoli <d4nkpersonal@gmail.com>
2019-07-27 14:10:57 -0400

    moved game comp to pages file

--- client/src/components/random-word/RandomWord.js
+++ client/src/components/random-word/RandomWord.js
@@ -2,3 +2,25 @@ import React, { Component } from "react";
 import "./RandomWord.css";
 
 
+class RandomWord extends Component {
+    state = {
+        loading: true
+    };
+
+    async componentDidMount(){
+        const url = "https://wordsapiv1.p.mashape.com/words?random=true";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({ drinks: data.drinks[0], loading: false });
+        console.log(data);
+    } 
+
+    render() {
+        return(
+            <div>
+                <p></p>
+            </div>
+        )
+    }
+
+}
