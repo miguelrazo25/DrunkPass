6c4bf105d52c4fe60cd32f148a7ce1a414b53a98
TacticalLoli <d4nkpersonal@gmail.com>
2019-08-03 11:07:27 -0400

    word matching check

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import Game from './components/pages/Game';
-import GetDrink from './components/getdrink/GetDrink';
-import Header from './components/header/Header'
 
 function App() {
   return (
--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -1,19 +1,52 @@
 import React, { Component } from "react";
 import "./Game.css"
 import Header from '../header/Header';
+import GetDrink from '../getdrink/GetDrink';
 var randomWords = require('random-words');
 
 class Game extends Component {
-    state = {
+    constructor(props) {
+        super(props)
+        this.state = {
         score: 0,
-        word: ""
-    };
+        word: "",
+        userInput: ""
+        };
+    }
 
-    async componentDidMount(){
+
+    handleSubmit = event => {
+        event.preventDefault()
+        console.log(`${this.state.userInput}`)
+        console.log(`${this.state.word}`)
+        if (this.state.word === this.state.userInput){
+            console.log('yes')
+            return <div>{GetDrink}</div>
+        } else {
+            console.log('no')
+        }
+    }
+
+    handleInputChange = event => {
+        this.setState({
+            userInput: event.target.value
+        })
+    }
+
+    componentDidMount(){
+        this.word();
+    }
+
+    word = () => {
+        // get word
         const getRandomWord = randomWords();
         this.setState({ word: getRandomWord })
         console.log(getRandomWord)
+        //show word to user
+        //hide word and let the user type it.
+
     }
+    
 
     render() {
         return(
@@ -23,11 +56,13 @@ class Game extends Component {
                     <div className="card-header">
                     Type the Word
                     </div>
+                    <form onSubmit={this.handleSubmit}>
                     <div className="card-body">
                         <h5 className="card-title">{this.state.word}</h5>
-                        <input className="form-control form-control-lg" type="text" placeholder="Type the Word"></input>
-                        <a className="btn btn-mod">Submit</a>
+                        <input className="form-control form-control-lg" type="text" value={this.state.userInput} onChange={this.handleInputChange} placeholder="Type the Word"></input>
+                        <button type="submit" className="btn btn-mod">Submit</button>
                     </div>
+                    </form>
                 </div>
             </div>
         )
