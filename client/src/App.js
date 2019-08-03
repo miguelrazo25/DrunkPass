2c0c13713f7089981f0da73496c939719db85600
Erwins <erwinssaget@gmail.com>
2019-08-03 11:37:21 -0400

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
-
 import Game from './components/pages/Game';
 import Landing from "./components/pages/Landing";
-
+import GetDrink from './components/pages/GetDrink';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Router>
+				<Route path="/drink" component={GetDrink} />
         <Route path="/game" component={Game}/>
-          <Route path="/" exact component={Landing}/>
+				<Route path="/" exact component={Landing}/>
       </Router>
     </div>
   );
--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -1,73 +1,76 @@
 import React, { Component } from "react";
-import "./Game.css"
+import { Redirect } from 'react-router-dom';
 import Header from '../header/Header';
-import GetDrink from '../getdrink/GetDrink';
 var randomWords = require('random-words');
 
 class Game extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-        score: 0,
-        word: "",
-        userInput: ""
-        };
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			score: 0,
+			word: "",
+			userInput: "",
+			redirectToDrink: false
+		};
+	}
 
 
-    handleSubmit = event => {
-        event.preventDefault()
-        console.log(`${this.state.userInput}`)
-        console.log(`${this.state.word}`)
-        if (this.state.word === this.state.userInput){
-            console.log('yes')
-            return <div>{GetDrink}</div>
-        } else {
-            console.log('no')
-        }
-    }
+	handleSubmit = event => {
+		event.preventDefault()
+		console.log(`${this.state.userInput}`)
+		console.log(`${this.state.word}`)
+		if (this.state.word === this.state.userInput){
+			this.setState({ redirectToDrink: true })
+		} else {
+			console.log('no')
+		}
+	}
 
-    handleInputChange = event => {
-        this.setState({
-            userInput: event.target.value
-        })
-    }
+	handleInputChange = event => {
+		this.setState({
+			userInput: event.target.value
+		})
+	}
 
-    componentDidMount(){
-        this.word();
-    }
+	componentDidMount(){
+		this.word();
+	}
 
-    word = () => {
-        // get word
-        const getRandomWord = randomWords();
-        this.setState({ word: getRandomWord })
-        console.log(getRandomWord)
-        //show word to user
-        //hide word and let the user type it.
+	word = () => {
+		// get word
+		const getRandomWord = randomWords();
+		this.setState({ word: getRandomWord })
+		console.log(getRandomWord)
+		//show word to user
+		//hide word and let the user type it.
 
-    }
-    
+	}
 
-    render() {
-        return(
-            <div>
-                <Header />
-                <div className="card game-div">
-                    <div className="card-header">
-                    Type the Word
-                    </div>
-                    <form onSubmit={this.handleSubmit}>
-                    <div className="card-body">
-                        <h5 className="card-title">{this.state.word}</h5>
-                        <input className="form-control form-control-lg" type="text" value={this.state.userInput} onChange={this.handleInputChange} placeholder="Type the Word"></input>
-                        <button type="submit" className="btn btn-mod">Submit</button>
-                    </div>
-                    </form>
-                </div>
 
-            </div>
-        )
-    }
+	render() {
+		if(this.state.redirectToDrink) {
+			return <Redirect to="/drink" />
+		}
+
+		return(
+			<div>
+				<Header />
+				<div className="card game-div">
+					<div className="card-header">
+						Type the Word
+					</div>
+					<form onSubmit={this.handleSubmit}>
+						<div className="card-body">
+							<h5 className="card-title">{this.state.word}</h5>
+							<input className="form-control form-control-lg" type="text" value={this.state.userInput} onChange={this.handleInputChange} placeholder="Type the Word"></input>
+							<button type="submit" className="btn btn-mod">Submit</button>
+						</div>
+					</form>
+				</div>
+
+			</div>
+		)
+	}
 }
 
 export default Game;
