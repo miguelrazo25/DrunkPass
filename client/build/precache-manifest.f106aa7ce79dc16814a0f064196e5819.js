3fdbc6dba32e507583faa9a73c412bfcdb129baa
TacticalLoli <d4nkpersonal@gmail.com>
2019-08-06 21:55:46 -0400

    gradient part 2

---
+++ client/build/precache-manifest.f106aa7ce79dc16814a0f064196e5819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35788710f9f9319db2c95039af4a6313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34d77bf11b1810958d71",
+    "url": "/static/css/main.56c36a7c.chunk.css"
+  },
+  {
+    "revision": "081cc2e625f1830d24f9",
+    "url": "/static/js/2.9446fc04.chunk.js"
+  },
+  {
+    "revision": "34d77bf11b1810958d71",
+    "url": "/static/js/main.79e751d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df6ed8bfbe77455cdedb90a11c22effc",
+    "url": "/static/media/palm.df6ed8bf.png"
+  }
+]);
